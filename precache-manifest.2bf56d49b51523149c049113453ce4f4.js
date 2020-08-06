1c9e8487dea373e0a983a6cd08dd93f0d06c6308
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2020-08-06 19:39:05 +0900

    Updates

---
+++ precache-manifest.2bf56d49b51523149c049113453ce4f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed9110bd183265785e2811ce82eba4a",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "8f21e07f80193b423e2a",
+    "url": "/movie_app/static/css/main.b7cf6848.chunk.css"
+  },
+  {
+    "revision": "109356d05735fcb9e967",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f21e07f80193b423e2a",
+    "url": "/movie_app/static/js/main.e9518d6a.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
